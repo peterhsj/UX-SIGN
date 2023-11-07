5105929813175e4288ba3075bec9d97d90d814f3
peterhsj <peterhsj@gmail.com>
2023-11-07 08:58:41 +0800

    first version

---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app-bar flat>
+    <v-app-bar-title>
+      <v-icon icon="mdi-circle-slice-6" />
+    </v-app-bar-title>
+    <v-btn to="/"> Home </v-btn>
+    <v-btn to="/about"> About </v-btn>
+    <v-spacer></v-spacer>
+    <v-btn to="/about"> About </v-btn>
+  </v-app-bar>
+</template>
+
+<script setup>
+// import { RouterLink } from "vue-router";
+</script>
